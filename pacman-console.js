8df8f3ff53e430cbfac807b3a9d8708bf12be4f5
cordell <cordell.perry@gmail.com>
2017-12-16 17:50:12 -0500

    collections in js

--- pacman-console.js
+++ pacman-console.js
@@ -1,10 +1,27 @@
-// console.log( 'Pac-Man eats Ghosts for lunch');
+// // console.log( 'Pac-Man eats Ghosts for lunch');
+//
+// var ghosts = ['inky', 'blinky', 'pinky', 'clyde'];
+//
+// console.log('there are' + ghosts.length + 'ghosts.');
+// console.log('their names are: ');
+// console.log(ghosts[0]);
+// console.log(ghosts[1]);
+// console.log(ghosts[2]);
+// console.log(ghosts[3]);
 
-var ghosts = ['inky', 'blinky', 'pinky', 'clyde'];
 
-console.log('there are' + ghosts.length + 'ghosts.');
-console.log('their names are: ');
-console.log(ghosts[0]);
-console.log(ghosts[1]);
-console.log(ghosts[2]);
-console.log(ghosts[3]);
+var inky = {};
+inky.color = 'cyan';
+inky.personality = 'shadow';
+console.log(inky);
+
+var ghosts = {inky: 'cyan', blinky: 'red', pinky: 'pink', clyde: 'orange'};
+console.log(ghosts);
+
+//dot notation
+console.log(ghosts.inky); // => 'Cyan'
+console.log(ghosts.blinky); // => 'Red'
+
+//squarebracket notation --- same as above
+console.log(ghosts['inky']); // => 'Cyan'
+console.log(ghosts['blinky']); // => 'Red'
