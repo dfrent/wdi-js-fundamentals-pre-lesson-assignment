913d39336c22eacc77c1665590759b4fb2ef8de4
cordell <cordell.perry@gmail.com>
2017-12-16 17:40:53 -0500

    JS collections

--- pacman-console.js
+++ pacman-console.js
@@ -1,1 +1,10 @@
-console.log( 'Pac-Man eats Ghosts for lunch');
+// console.log( 'Pac-Man eats Ghosts for lunch');
+
+var ghosts = ['inky', 'blinky', 'pinky', 'clyde'];
+
+console.log('there are' + ghosts.length + 'ghosts.');
+console.log('their names are: ');
+console.log(ghosts[0]);
+console.log(ghosts[1]);
+console.log(ghosts[2]);
+console.log(ghosts[3]);
