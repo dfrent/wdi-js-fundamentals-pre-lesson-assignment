681231f20079f623d81349e13e8090a8b40db464
cordell <cordell.perry@gmail.com>
2017-12-16 17:33:38 -0500

    ruby reference code

---
+++ pacman.rb
@@ -0,0 +1,8 @@
+# pacman.rb
+ghosts = ['Inky', 'Blinky', 'Pinky', 'Clyde']
+puts "There are #{ghosts.length}  ghosts."
+puts 'Their names are: '
+puts ghosts[0]
+puts ghosts[1]
+puts ghosts[2]
+puts ghosts[3]
